5773a21db7e5e607052b6c68016e1541cdfb9eac
”Dmitri <dmitri.zd1958@gmail.com>
2024-04-28 16:33:39 +0100

    #1 Init commit

---
+++ src/main/java/com/weektwit/auth/WeektwitauthApplication.java
@@ -0,0 +1,13 @@
+package com.weektwit.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeektwitauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeektwitauthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/weektwit/auth/WeektwitauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weektwit.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeektwitauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
